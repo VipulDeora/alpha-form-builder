9ff8dc55c1a57d9a1ed5934b500e1369e76f325a
vipulGoals101 <vipul.deora@goals101.in>
2020-07-22 18:00:35 +0530

    initial commit

---
+++ src/app/field-picker/field-picker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-field-picker',
+  templateUrl: './field-picker.component.html',
+  styleUrls: ['./field-picker.component.scss']
+})
+export class FieldPickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
